b5288dee74b2279aed253490fb9014ed8efc7247
dani4110 <freakoutcoding@gmail.com>
2023-11-28 06:44:02 -0500

    exercise 1

---
+++ JavaScript-Challenge-Bucles.js
@@ -0,0 +1,42 @@
+// let array = [ 'River Plate', ' Arsenal', 'PSG', 'Barcelona', 'Inter de Miami'];
+// for (let i = 0; i < array.length; i++ ) {
+//     let equipos = array[i];
+//     if (equipos === 'River Plate' || equipos === 'Arsenal' || equipos === 'Barcelona'){
+//         console.log ( equipos + " Teams Alexis Sanchez has played for.");
+//     } else if 
+//          (equipos === 'PSG' || equipos === 'Barcelona' || equipos === 'Inter de Miami'){
+//             console.log ( equipos + " Teams Lionel Messi has played for."); 
+//          }
+//     };
+
+
+const colors = ['red','green','blue','yellow'];
+const person =  {
+    name : 'María',
+    age : '27',
+    profession : 'diseñadora'
+}
+
+// 1.- Print the numbers from 1 to 5
+
+let i = 0;
+while (i<=5) {
+    console.log(i);
+    i++;
+}
+
+// 2.-Go through a list of colors.
+
+for ( let i = 0; i < colors.length; i++){
+console.log(colors[i]);
+}
+
+
+
+
+
+      
+
+
+    
+
